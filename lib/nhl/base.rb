425dddcbe87b0b0dce0316b90297703f170c257f
sherwin-kwan <sherwin.kwan92@gmail.com>
2023-04-16 20:31:42 -0700

    update NHL logo source (they changed the path)

--- lib/nhl/base.rb
+++ lib/nhl/base.rb
@@ -3,7 +3,7 @@ require "nhl/version"
 module NHL
   BASE = "https://statsapi.web.nhl.com/api/v1/"
   MAIN_CDN = "https://nhl.bamcontent.com/images/"
-  LOGO_CDN = "https://www-league.nhlstatic.com/builds/site-core/86d4b76cc03a4d111ee0e20f9f62eb054eef3b74_1502985652/images/logos/team/current/"
+  LOGO_CDN = "https://www-league.nhlstatic.com/nhl.com/builds/site-core/af6a3b2b107fd6b9f8dcca343d039a1e3297f5bc_1678480913/images/logos/team/current/"
 end
 
 require "nhl/conference"
