e817871d98868d14df9dc9855a3b4eaef857bbf6
Sherwin Kwan <sherwin.kwan92@gmail.com>
2024-04-18 23:38:47 -0700

    update NHL API address

--- lib/nhl/base.rb
+++ lib/nhl/base.rb
@@ -1,7 +1,7 @@
 require "nhl/version"
 
 module NHL
-  BASE = "https://statsapi.web.nhl.com/api/v1/"
+  BASE = "https://api-web.nhle.com/v1/"
   MAIN_CDN = "https://nhl.bamcontent.com/images/"
   LOGO_CDN = "https://www-league.nhlstatic.com/nhl.com/builds/site-core/af6a3b2b107fd6b9f8dcca343d039a1e3297f5bc_1678480913/images/logos/team/current/"
 end
